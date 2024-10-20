fe40aa5ed0e07940405ae273b7da5dd3bc400908
G-Machado <gabriel.machado.cg@gmail.com>
2024-10-20 20:00:31 -0300

    barrel animation

--- Assets/Scripts/BumperController.cs
+++ Assets/Scripts/BumperController.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class BumperController : MonoBehaviour
 {
-
-
     [SerializeField] private float bumpForceAmmount = 7;
-
-
+    [SerializeField] private Animator animator;
 
     public float GetBumpForce() {
+
+        if(animator)
+            animator.Play("Barrel_Collision");
         return bumpForceAmmount;
     }
 
